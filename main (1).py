d839000f65f2313b496ce4f2e9b485288ad6a0f5
NemoCpit <cpitservicesandsolutions@gmail.com>
2025-06-23 19:35:25 +0530

    Add Day 1 NumPy vector practice
    This file contains basic NumPy operations:
    - Creating vectors
    - Adding two vectors
    - Scalar multiplication
    Done as part of Week 2, Day 1 AI learning project.

---
+++ main (1).py
@@ -0,0 +1,9 @@
+import numpy as np
+# Create a Numpy vector
+v1 = np.array([5, 2])
+print("Vector v1:", v1)
+v2 = np.array([3, 7])
+result = v1 + v2
+print("v1 + v2 =", result)
+scaled = 2 * v1  
+print ("2 * v1 =", scaled)
